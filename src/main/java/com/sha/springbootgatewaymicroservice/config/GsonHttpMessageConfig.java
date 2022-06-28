366c21a59fabd2f60ca217178cb27055c76b415e
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-29 02:22:29 +0300

    local datetime message not readable bug fix

--- src/main/java/com/sha/springbootgatewaymicroservice/config/GsonHttpMessageConfig.java
+++ src/main/java/com/sha/springbootgatewaymicroservice/config/GsonHttpMessageConfig.java
@@ -5,30 +5,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
-public class GsonHttpMessageConfig {
+public class GsonHttpMessageConfig
+{
     @Bean
     public GsonBuilder gsonBuilder()
     {
         return new GsonBuilder()
-                .registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>)(date, type, context) ->
-                        date == null ? null : new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE)))
-                .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>)(json, type, context) ->
-                        LocalDateTime.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE));
+                .registerTypeAdapter(LocalDate.class,
+                        (JsonSerializer<LocalDate>) (date, type, context) ->
+                                date == null ? null : new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE)))
+                .registerTypeAdapter(LocalDate.class,
+                        (JsonDeserializer<LocalDate>) (json, type, context) ->
+                                LocalDate.parse(json.getAsString(), DateTimeFormatter.ISO_LOCAL_DATE));
     }
 
     @Bean
-    public Gson gson(GsonBuilder gsonBuilder){
+    public Gson gson(GsonBuilder gsonBuilder)
+    {
         return gsonBuilder.create();
     }
 
     @Bean
-    public GsonHttpMessageConverter gsonHttpMessageConverter(Gson gson){
-        GsonHttpMessageConverter gsonHttpMessageConverter = new GsonHttpMessageConverter();
-        gsonHttpMessageConverter.setGson(gson);
-        return gsonHttpMessageConverter;
+    public GsonHttpMessageConverter gsonHttpMessageConverter(Gson gson)
+    {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        converter.setGson(gson);
+        return converter;
     }
 }
