cc3516dda667fd4562c7b3ba1b9f74b22bf7c5c5
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-28 16:38:40 +0300

    set user repository interface operations

---
+++ src/main/java/com/sha/springbootgatewaymicroservice/Repository/IUserRepository.java
@@ -0,0 +1,12 @@
+package com.sha.springbootgatewaymicroservice.Repository;
+
+import com.sha.springbootgatewaymicroservice.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
