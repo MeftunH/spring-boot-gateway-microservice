eebdcf476033b61e2d77dab4506dcb042e07b22d
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-28 23:15:19 +0300

    auth manager bean init

--- src/main/java/com/sha/springbootgatewaymicroservice/Security/SecurityConfig.java
+++ src/main/java/com/sha/springbootgatewaymicroservice/Security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
