1969a2612b57311db788a963eed5912d939bb753
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-28 22:48:06 +0300

    token validation bug fix

--- src/main/java/com/sha/springbootgatewaymicroservice/Security/jwt/JwtProvider.java
+++ src/main/java/com/sha/springbootgatewaymicroservice/Security/jwt/JwtProvider.java
@@ -27,37 +27,52 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class JwtProvider implements IJwtProvider {
-
-    private static final String JWT_TOKEN_PREFIX = "Bearer ";
-    private static final String JWT_HEADER_STRING = "Authorization";
-    private static final PrivateKey jwtPrivateKey = null;
-    private static final PublicKey jwtPublicKey = null;
+public class JwtProvider implements IJwtProvider
+{
     @Value("${authentication.jwt.expiration-in-ms}")
     private Long JWT_EXPIRATION_IN_MS;
 
-    public JwtProvider(@Value("${authentication.jwt.public-key}") String jwtPublicKey,
-                       @Value("${authentication.jwt.private-key}") String jwtPrivateKey) {
-        try {
-            KeyFactory keyFactory = getKeyFactory();
+    private static final String JWT_TOKEN_PREFIX = "Bearer";
+    private static final String JWT_HEADER_STRING = "Authorization";
+
+    private final PrivateKey jwtPrivateKey;
+    private final PublicKey jwtPublicKey;
+
+    /*
+      First, we require public and private keys for RSA encryption and decryption.
+      Hence, below is the tool to generate RSA key online.
+      It generates RSA public key as well as the private key of size 512 bit, 1024 bit, 2048 bit,
+      3072 bit and 4096 bit with Base64 encoded.
+      By default, the private key is generated in PKCS#8 format and the public key is generated in X.509 format.
+     */
+    public JwtProvider(@Value("${authentication.jwt.private-key}") String jwtPrivateKeyStr,
+                       @Value("${authentication.jwt.public-key}") String jwtPublicKeyStr)
+    {
+        KeyFactory keyFactory = getKeyFactory();
+
+        try
+        {
             Base64.Decoder decoder = Base64.getDecoder();
-            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(decoder.decode(jwtPrivateKey.getBytes()));
-            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(decoder.decode(jwtPublicKey.getBytes()));
-            jwtPrivateKey = String.valueOf(keyFactory.generatePrivate(privateKeySpec));
-            jwtPublicKey = String.valueOf(keyFactory.generatePrivate(publicKeySpec));
-        } catch (InvalidKeySpecException e) {
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(decoder.decode(jwtPrivateKeyStr.getBytes()));
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(decoder.decode(jwtPublicKeyStr.getBytes()));
+
+            jwtPrivateKey = keyFactory.generatePrivate(privateKeySpec);
+            jwtPublicKey = keyFactory.generatePublic(publicKeySpec);
+        }
+        catch (Exception e)
+        {
             throw new RuntimeException("Invalid key specification", e);
         }
     }
 
     @Override
-    public String generateToken(UserPrincipal authentication) {
+    public String generateToken(UserPrincipal authentication)
+    {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining());
 
-        return Jwts.builder()
-                .setSubject(authentication.getUsername())
+        return Jwts.builder().setSubject(authentication.getUsername())
                 .claim("userId", authentication.getId())
                 .claim("roles", authorities)
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_IN_MS))
@@ -66,13 +81,19 @@ public class JwtProvider implements IJwtProvider {
     }
 
     @Override
-    public Authentication getAuthentication(HttpServletRequest request) {
+    public Authentication getAuthentication(HttpServletRequest request)
+    {
         String token = resolveToken(request);
-        if (token != null) {
+        if (token == null)
+        {
             return null;
         }
-        Claims claims = Jwts.parserBuilder().setSigningKey(jwtPublicKey)
-                .build().parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(jwtPublicKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
         String username = claims.getSubject();
         Long userId = claims.get("userId", Long.class);
         List<GrantedAuthority> authorities = Arrays.stream(claims.get("roles").toString().split(","))
@@ -80,38 +101,50 @@ public class JwtProvider implements IJwtProvider {
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
 
-        UserDetails userDetails = new UserPrincipal(userId , username, null);
+        UserDetails userDetails = new UserPrincipal(userId, username, null);
         return username != null ? new UsernamePasswordAuthenticationToken(userDetails, null, authorities) : null;
     }
 
-    private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(JWT_HEADER_STRING);
-        if (bearerToken != null && bearerToken.startsWith(JWT_TOKEN_PREFIX)) {
-            return bearerToken.substring(JWT_TOKEN_PREFIX.length());
-        }
-        return null;
-    }
-
     @Override
-    public boolean isTokenValid(HttpServletRequest request){
+    public boolean isTokenValid(HttpServletRequest request)
+    {
         String token = resolveToken(request);
-        if (token != null) {
+        if (token == null)
+        {
             return false;
         }
-        Claims claims = Jwts.parserBuilder().setSigningKey(jwtPublicKey)
-                .build().parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(jwtPublicKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
 
-        if (claims.getExpiration().before(new Date())) {
+        if (claims.getExpiration().before(new Date()))
+        {
             return false;
         }
         return true;
     }
 
-    private KeyFactory getKeyFactory() {
-        try {
+    private String resolveToken(HttpServletRequest request)
+    {
+        String bearerToken = request.getHeader(JWT_HEADER_STRING);
+        if (bearerToken != null && bearerToken.startsWith(JWT_TOKEN_PREFIX))
+        {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    private KeyFactory getKeyFactory()
+    {
+        try
+        {
             return KeyFactory.getInstance("RSA");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Unknown algorithm", e);
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException("Unknown key generation algorithm", e);
         }
     }
 }
