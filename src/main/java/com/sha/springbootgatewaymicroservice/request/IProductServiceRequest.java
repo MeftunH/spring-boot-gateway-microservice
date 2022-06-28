202d107762114430d97b7fb0015666af19decd92
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-29 01:16:11 +0300

    set retrofit endpoints for product sub service

---
+++ src/main/java/com/sha/springbootgatewaymicroservice/request/IProductServiceRequest.java
@@ -0,0 +1,19 @@
+package com.sha.springbootgatewaymicroservice.request;
+
+import com.google.gson.JsonElement;
+import lombok.Getter;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+
+public interface IProductServiceRequest {
+    @POST("api/v1/product")
+    Call<JsonElement> saveProduct(@Body JsonElement requestBody);
+
+    @DELETE("api/v1/product/{id}")
+    Call<Void> deleteProduct(@Path("id") Long id);
+
+    @GET("api/v1/product")
+    Call<List<JsonElement>> getAllProducts();
+}
