31d7e098e54411efc78ebddec211123e3b267790
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-29 02:43:34 +0300

    fix product endpoint

--- src/main/java/com/sha/springbootgatewaymicroservice/request/IProductServiceRequest.java
+++ src/main/java/com/sha/springbootgatewaymicroservice/request/IProductServiceRequest.java
@@ -8,12 +8,12 @@ import retrofit2.http.*;
 import java.util.List;
 
 public interface IProductServiceRequest {
-    @POST("api/v1/product")
+    @POST("api/v1/products")
     Call<JsonElement> saveProduct(@Body JsonElement requestBody);
 
-    @DELETE("api/v1/product/{id}")
+    @DELETE("api/v1/products/{id}")
     Call<Void> deleteProduct(@Path("id") Long id);
 
-    @GET("api/v1/product")
+    @GET("api/v1/products")
     Call<List<JsonElement>> getAllProducts();
 }
--- src/main/java/com/sha/springbootgatewaymicroservice/utils/RetrofitUtils.java
+++ src/main/java/com/sha/springbootgatewaymicroservice/utils/RetrofitUtils.java
@@ -1,5 +1,6 @@
 package com.sha.springbootgatewaymicroservice.utils;
 
+import com.google.gson.JsonElement;
 import lombok.extern.slf4j.Slf4j;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -7,16 +8,23 @@ import retrofit2.Response;
 import java.io.IOException;
 
 @Slf4j
-public class RetrofitUtils {
-    public static <T> T executeInBlock(Call<T> request) {
-        try {
+public class RetrofitUtils
+{
+    public static <T> T executeInBlock(Call<T> request)
+    {
+        try
+        {
             Response<T> response = request.execute();
-            if (!response.isSuccessful()) {
-                log.error("Error while saving product with statusCode: {} and reason:{}",
+
+            if (!response.isSuccessful())
+            {
+                log.error("Request completed unsuccessfully with statusCode:{} and reason:{}",
                         response.code(), response.errorBody().string());
             }
             return response.body();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             throw new RuntimeException(e.getCause());
         }
     }
